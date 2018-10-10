20240fe3a78ea1c1cdee834bb7e8bcbec0862b0f
nox757 <nox757@gmail.com>
2018-10-10 09:58:14 +0300

    Генератор файла xls со случайными пользователями.
    * Первоначальная структура

---
+++ src/main/java/fintechQA/Main.java
@@ -0,0 +1,37 @@
+package fintechQA;
+
+import org.apache.commons.lang3.RandomUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static fintechQA.RandomUtilsGenerator.randString;
+
+public class Main {
+
+    public static List<String> headers = new ArrayList<>(Arrays.asList("Фамилия",
+            "Имя", "Отчество", "Возраст", "Пол", "Дата рождения", "Инн",
+            "Почтовый индекс", "Страна", "Область", "Город", "Улица", "Дом", "Квартира"));
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        RandomUtilsGenerator randomUtilsGenerator = new RandomUtilsGenerator();
+        randomUtilsGenerator.fillResources();
+        RandomPersonGenerator personGenerator = new RandomPersonGenerator(randomUtilsGenerator);
+        List<Person> persons = new ArrayList<>();
+        int numRow = RandomUtils.nextInt(1,30);
+        for (int i = 0; i < numRow; i++) {
+            persons.add(personGenerator.generate());
+        }
+        ExcelCreator excelCreator = new ExcelCreator("new");
+        excelCreator.createHeaderRow(headers);
+        excelCreator.addRows(persons);
+        excelCreator.writeToFile(randString());
+        //TODO: сделать вывод имени файла и добавить README
+        System.out.println();
+    }
+}
---
+++ src/main/java/fintechQA/RandomPersonGenerator.java
@@ -0,0 +1,55 @@
+package fintechQA;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
+
+import java.time.LocalDate;
+
+public class RandomPersonGenerator {
+
+    private RandomUtilsGenerator rdUtils;
+
+    public RandomPersonGenerator(RandomUtilsGenerator rdUtils) {
+        this.rdUtils = rdUtils;
+    }
+
+    public Person generate() {
+        Person person = new Person();
+        person.setGender(genereteGender());
+        fillNames(person);
+        person.setCountry(rdUtils.randCountry())
+                .setRegion(rdUtils.randRegion())
+                .setCity(rdUtils.randCity())
+                .setStreet(rdUtils.randStreet())
+                .setPostCode(RandomStringUtils.randomNumeric(6))
+                .setNumHouse(RandomUtils.nextInt(0, 999))
+                .setNumFlat(RandomUtils.nextInt(0,999))
+                .setInn(generateInn())
+                .setBirthday(LocalDate.now().minusYears(RandomUtils.nextLong(1, 100))); //TODO: сдеалть генерацию разных дат
+        return person;
+    }
+
+    private String genereteGender() {
+        return RandomUtils.nextInt(0, 2) == 1 ? "М" : "Ж";
+    }
+
+    private void fillNames(Person person) {
+        if (person.getGender().equals("М")) {
+            person.setSurname(rdUtils.randManSurname())
+                    .setName(rdUtils.randManName())
+                    .setMiddleName(rdUtils.randManMiddleName());
+        }
+        if (person.getGender().equals("Ж")) {
+            person.setSurname(rdUtils.randWomanSurname())
+                    .setName(rdUtils.randWomanName())
+                    .setMiddleName(rdUtils.randWomanMiddleName());
+        }
+    }
+
+    private String generateInn() {
+        String inn = "77";
+        //TODO: сделать валидную генерацию ИНН
+        inn += RandomStringUtils.randomNumeric(10);
+        return inn;
+    }
+}
