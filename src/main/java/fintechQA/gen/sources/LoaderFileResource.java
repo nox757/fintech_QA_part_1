9fd9e4dfe670f574f58459f8c43f270616f6383f
nox757 <nox757@gmail.com>
2018-10-20 17:03:26 +0300

    Поправлена работа с ресурсами. Добвален файл README

--- src/main/java/fintechQA/gen/sources/LoaderFileResource.java
+++ src/main/java/fintechQA/gen/sources/LoaderFileResource.java
@@ -2,8 +2,12 @@ package fintechQA.gen.sources;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -13,7 +17,7 @@ import java.util.stream.Stream;
 
 public class LoaderFileResource {
 
-    public static List<String> loadResource(String fileName) {
+    public static List<String> loadResource2(String fileName) {
         List<String> list = new ArrayList<>();
         try (Stream<String> stream = Files.lines(Paths.get(ClassLoader.getSystemResource(fileName).toURI()))) {
             list = stream
@@ -21,6 +25,17 @@ public class LoaderFileResource {
                     .collect(Collectors.toList());
         } catch (URISyntaxException | IOException ex) {
             ex.printStackTrace();
+        }
+        return list;
+    }
+
+    public static List<String> loadResource(String fileName) {
+        List<String> list;
+        InputStream in = ClassLoader.getSystemResourceAsStream(fileName);
+        try (Stream<String> stream = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8)).lines()) {
+            list = stream
+                    .filter(StringUtils::isNotBlank)
+                    .collect(Collectors.toList());
         }
         return list;
     }
